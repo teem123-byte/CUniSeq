c4df2a28ad9ecb45a9d15f66294d721d353fc2d4
teem123-byte <67989779+teem123-byte@users.noreply.github.com>
2021-04-19 11:52:17 -0500

    Update CUniSeq.R
    weird output error, it now works.

--- CUniSeq.R
+++ CUniSeq.R
@@ -1,4 +1,3 @@
-
 library(seqinr)
 library(seqRFLP)
 library(Biostrings)
@@ -6,14 +5,14 @@ library(msa)
 library(DECIPHER)
 library(plyr)
 
-genes<-readDNAStringSet('/Users/hamiji/Desktop/Convictupdates/uniques/targetDB_nt_cons.fasta')
+genes<-readDNAStringSet('/Users/hamiji/Downloads/targetDB_nt_cons.fasta')
 genes_compare<-genes
 CS<-data.frame()
 for(j in 1:length(genes_compare)){
   gene<-1
   gene_2<-2
-    for(i in 1:length(genes_compare)){
-      if(gene_2<=length(genes_compare)){
+  for(i in 1:length(genes_compare)){
+    if(gene_2<=length(genes_compare)){
       comparison<-paste(names(genes_compare[gene]), "/", names(genes_compare[gene_2]))
       MA<-msa(genes_compare[c(gene,gene_2)], method = "ClustalOmega")
       MA<-DNAStringSet(MA)
@@ -24,9 +23,9 @@ for(j in 1:length(genes_compare)){
       CS_1<-paste(CS_1, sep = "", collapse = "")
       CS_1<-cbind(comparison,CS_1)
       CS<-rbind(CS, CS_1)
-      }
-      gene_2<-gene_2+1
     }
+    gene_2<-gene_2+1
+  }
   genes_compare<-genes_compare[-1]
 }
 
@@ -53,7 +52,10 @@ UniqueConservedSequence<-function(CS, DistanceThreshold, MinSeqLength){
       seq[[seqcnt]]<-seq1
       print(seq[[seqcnt]])
       if(seqcnt !=1){
-      currentpos_prev<-as.numeric(seq[[seqcnt-1]][4])
+        currentpos_prev<-as.numeric(seq[[seqcnt-1]][4])
+      }
+      if(seqcnt==1){
+        currentpos_prev<-0
       }
       if(currentpos_prev==currentposition) {break}
       seqcnt<-seqcnt+1
@@ -69,10 +71,17 @@ UniqueConservedSequence<-function(CS, DistanceThreshold, MinSeqLength){
 
 #starting and ending positiions are wrong
 
-teh<-CS[c(16:21,9,2),]
+teh<-CS
+
+output_list<-list()
+for(i in 1:nrow(teh)){
+  input<-unlist(strsplit(teh$CS_1[i],""))
+  v<-UniqueConservedSequence(input,10,100)
+  output_list[[i]]<-v
+}
 
 
 
-inputseq<-strsplit(teh[5,2], "")[[1]]
-test<-UniqueConservedSequence(inputseq,5,100)
+
+
 
